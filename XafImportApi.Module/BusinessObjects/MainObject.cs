0c4619f26fc4835da154fbaaac0c39c707e355a9
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-10-11 01:39:52 +0300

    Add project files.

---
+++ XafImportApi.Module/BusinessObjects/MainObject.cs
@@ -0,0 +1,60 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XafImportApi.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class MainObject : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public MainObject(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        bool active;
+        DateTime date;
+        string name;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Name
+        {
+            get => name;
+            set => SetPropertyValue(nameof(Name), ref name, value);
+        }
+
+        public DateTime Date
+        {
+            get => date;
+            set => SetPropertyValue(nameof(Date), ref date, value);
+        }
+        
+        public bool Active
+        {
+            get => active;
+            set => SetPropertyValue(nameof(Active), ref active, value);
+        }
+    }
+}
---
+++ XafImportApi.Module/Import/ImportHelper.cs
@@ -0,0 +1,83 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Pdf.Native.BouncyCastle.Utilities;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DevExpress.Drawing.Printing.Internal.DXPageSizeInfo;
+
+namespace XafImportApi.Module.Import
+{
+    public enum PropertyKind
+    {
+        Primitive, Reference, Enum,
+
+    }
+    public class PropertyInfo
+    {
+        public string Name { get; set; }
+        public string PropertyType { get; set; }
+        public PropertyKind PropertyKind { get; set; }
+    }
+    public class RowDef
+    {
+
+        public Dictionary<int, PropertyInfo> Properties { get; set; }
+        public List<List<object>> Rows { get; set; }
+    }
+
+
+    public class ImportHelper
+    {
+        void Import(IObjectSpace objectSpace, RowDef rowDef)
+        {
+            List<KeyValuePair<int, PropertyInfo>> RefProperties = rowDef.Properties.Where(p => p.Value.PropertyKind == PropertyKind.Reference).ToList();
+            var XpOs = objectSpace as XPObjectSpace;
+            ITypesInfo TypesInfo = XpOs.TypesInfo;
+            var PageSize = 1000;
+            var Pages = GetPageSize(rowDef, PageSize);
+            Dictionary<PropertyInfo, XPCollection> Collections = new Dictionary<PropertyInfo, XPCollection>();
+            foreach (KeyValuePair<int, PropertyInfo> RefProp in RefProperties)
+            {
+                Collections.Add(RefProp.Value, BuildCollection(RefProp, rowDef, XpOs.Session, TypesInfo, PageSize, Pages));
+            }
+            XpOs.Session.BulkLoad(Collections.Select(c => c.Value).ToArray());
+
+        }
+        XPCollection BuildCollection(KeyValuePair<int, PropertyInfo> RefProperties, RowDef rowDef, Session session, ITypesInfo TypesInfo, int pageSize, int pages)
+        {  
+            var Criteria=BuildCriteria(RefProperties, rowDef, pageSize, pages);
+            return new XPCollection(PersistentCriteriaEvaluationBehavior.BeforeTransaction, session, TypesInfo.FindTypeInfo(RefProperties.Value.PropertyType).Type, Criteria);
+        }
+        CriteriaOperator BuildCriteria(KeyValuePair<int, PropertyInfo> RefProperties, RowDef rowDef, int pageSize, int pages)
+        {
+
+            List<CriteriaOperator> operators = new List<CriteriaOperator>();
+            for (int i = 0; i < pages; i++)
+            {
+                operators.Add(new InOperator(RefProperties.Value.Name, GetValues(rowDef, i, pageSize, RefProperties.Key)));
+            }
+            return CriteriaOperator.Or(operators);
+           
+        }
+
+        int GetPageSize(RowDef rowDef, int pageSize)
+        {
+            return (int)Math.Ceiling((double)rowDef.Rows.Count() / pageSize);
+        }
+        List<Object> GetValues(RowDef rowDef, int page, int pageSize, int ColumIndex)
+        {
+            return rowDef.Rows
+                .Select(innerList => innerList[ColumIndex])
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+        }
+    }
+}
+
